c58fe74ff6de14974dbc00961b9b4ba763d2dfd6
James <jhandshoe@sbcglobal.net>
2016-07-11 14:59:11 -0400

    added Ryan's awesome images!

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,1 +1,18 @@
-$('.cycle-slideshow').cycle();
+$(document).ready(function(){
+	$('#s3').cycle({ 
+    	fx:    'fade', 
+    	speed:  1500 
+ 	});
+
+ 	$('#monthlyBudget').click(function(){
+ 		window.open("http://codepen.io/jhandshoe/pen/rLmgrr");
+ 	});
+
+ 	$('#ajaxTutorial').click(function(){
+ 		window.open("ajax tutorial/index.html");
+ 	});
+
+ 	$('#meditationApp').click(function(){
+ 		window.open("meditation app/index.html");
+ 	});
+});
